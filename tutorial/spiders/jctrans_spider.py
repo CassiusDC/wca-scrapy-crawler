75e3a6acc4e02bcc9f5cd01fd813772ee31be995
cassiusdc <cassiusdelacruz@gmail.com>
2021-05-12 16:00:09 +0800

    JCTrans crawl all countries

--- tutorial/spiders/jctrans_spider.py
+++ tutorial/spiders/jctrans_spider.py
@@ -2,7 +2,7 @@ import scrapy
 import re
 from scrapy import signals
 from pydispatch import dispatcher
-from scrapy.http import FormRequest
+from scrapy.http import FormRequest, request
 from scrapy.http import Request
 import pandas as pd
 import time
@@ -11,131 +11,22 @@ from scrapy.shell import inspect_response
 from itemadapter import ItemAdapter
 import json
 #Declare variables to be exported
-company_country = []
-company_email = []
-company_name = []
-no_of_employees = []
-address = []
-company_phone = []
-company_contact_person = []
-company_website = []
-contact_person_name = []
-contact_person2_name = []
-contact_person3_name = []
-contact_person4_name = []
-contact_person_title = []
-contact_person2_title = []
-contact_person3_title = []
-contact_person4_title = []
-contact_person_phone = []
-contact_person2_phone = []
-contact_person3_phone = []
-contact_person4_phone = []
-contact_person_email = []
-contact_person2_email = []
-contact_person3_email = []
-contact_person4_email = []
-company_contact_person_title = []
+
 PAGES = [
-    
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-1.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-2.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-3.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-4.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-5.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-6.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-7.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-8.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-9.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-10.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-11.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-12.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-13.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-14.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-15.html",
-    "https://www.jctrans.net/company/list-0-0-australia-0-3-0-0-0-16.html",
-
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-1.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-2.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-3.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-4.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-5.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-6.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-7.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-8.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-9.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-10.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-11.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-12.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-13.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-14.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-15.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-16.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-17.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-18.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-19.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-20.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-21.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-22.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-23.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-24.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-25.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-26.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-27.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-28.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-29.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-30.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-31.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-32.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-33.html",
-    "https://www.jctrans.net/company/list-0-0-malaysia-0-3-0-0-0-34.html",
-    
-    "https://www.jctrans.net/company/list-0-0-new%20zealand-0-3-0-0-0-1.html",
-    "https://www.jctrans.net/company/list-0-0-new%20zealand-0-3-0-0-0-2.html",
-
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-1.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-2.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-3.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-4.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-5.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-6.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-7.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-8.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-9.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-10.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-11.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-12.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-13.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-14.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-15.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-16.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-17.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-18.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-19.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-20.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-21.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-22.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-23.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-24.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-25.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-26.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-27.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-28.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-29.html",
-    "https://www.jctrans.net/company/list-0-0-singapore-0-3-0-0-0-30.html",
+    "https://www.jctrans.net/company/index.html",
+    "https://www.jctrans.net/company/gsk_china.html",
 
 
 
 ]
-class JCTransSpider(scrapy.Spider): #neiw
+class JCTransSpider(scrapy.Spider): 
     name = "jctrans"
     start_urls = ['https://www.jctrans.net/Login/Login.html',]
-       
-        
+ 
 #SPIDER START      
     #Loop through directory listings
     def parse(self, response):
-        form_data = {"UserName":"Vicasso","PassWord":"Vicasso","Remember":"0"}
+        form_data = {"UserName":"Vicasso","PassWord":"Vicasso","Remember":"1"}
         headers = {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
                     'Connection': 'keep-alive',
                     'X-Requested-With': 'XMLHttpRequest',
@@ -161,16 +52,41 @@ class JCTransSpider(scrapy.Spider): #neiw
                             callback=self.parse_after_login,
                             )
                             
-    
+
+        
+
+
     def parse_after_login(self, response):
         for url in PAGES:
-            yield scrapy.Request(url, callback=self.parse_page)
+            return scrapy.Request(url, callback=self.parse_after_login_country_listing)
+        
+    def parse_after_login_country_listing(self, response):
+        COUNTRY_SELECTOR = "div > ul > a::attr(href)"
+        
+        print("Following " + response.css(COUNTRY_SELECTOR).extract_first())
+        list = response.css(COUNTRY_SELECTOR)
+        print(list)
+        yield from response.follow_all(list, self.parse_page)
+
+
 
     def parse_page(self, response):
         SET_SELECTOR = 'div.left_contenty > div > div > div.col-md-7.center > div.titlel > a::attr(href)'
-        yield from response.follow_all(response.css(SET_SELECTOR), self.parse_listing)
+        NEXT_PAGE_SELECTOR = '/html/body/div/div/div/div/div/div/div/div/ul/li/a[contains(string(),"Next Page")]/@href'
+        SET_LOGIN_SELECTOR='//*[@id="dl"]/span[contains(text(),"SIGN IN")]'
 
+        companies = response.css(SET_SELECTOR).extract()
+        print(companies)
+        start_url = "https://www.jctrans.net/"
+        for company in companies:
+            company_url = start_url + company
+            yield scrapy.Request(company_url, self.parse_listing)
         
+        url = response.xpath(NEXT_PAGE_SELECTOR).extract_first()
+        if url:
+            print("GOING TO NEXT PAGE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            yield scrapy.Request(url,self.parse_page)
+
     #Find information, append to array
     def parse_listing(self, response):
         print("DETECTED: " + str(response.css('#navright > div.link_login.entry > a::text').extract()) + " BUTTON IN COMPANY PAGE")
@@ -188,19 +104,6 @@ class JCTransSpider(scrapy.Spider): #neiw
         COMPANY_EMAIL_SELECTOR = '//*[@id="home"]/div/div[1]/div/div/div/div[contains(string(),"E-mall:")]/following-sibling::div[1]/node()[self::text()]'
         COMPANY_LINKEDIN_SELECTOR = '#home > div.tab-p.kly > div > div.zlf > div.Follow > ul > li.tj4 > a::attr(href)'
 
-
-        company_country = response.xpath(COMPANY_COUNTRY_SELECTOR).extract()
-        company_name = response.css(COMPANY_SELECTOR).extract()
-        no_of_employees = response.xpath(no_of_employees_SELECTOR).extract()
-        address = response.xpath(ADDRESS_SELECTOR).extract()
-
-
-        company_phone = response.xpath(COMPANY_CONTACT_NUMBER_SELECTOR).extract()
-        company_contact_person = response.xpath(COMPANY_CONTACT_PERSON_SELECTOR).extract_first()
-        company_contact_person_title = response.xpath(COMPANY_CONTACT_PERSON_TITLE_SELECTOR).extract()
-        company_website = response.xpath(COMPANY_WEBSITE_SELECTOR).extract()
-        company_email = response.xpath(COMPANY_EMAIL_SELECTOR).extract()
-        company_linkedin = response.css(COMPANY_LINKEDIN_SELECTOR).extract()
         # #Contact Person Loop
         contact_loop_ctr = 1
 
@@ -235,16 +138,16 @@ class JCTransSpider(scrapy.Spider): #neiw
             
             contact_loop_ctr+=1
         yield{
-            "company_country": company_country,
-            "company_name": company_name,
-            "no_of_employees": no_of_employees,
-            "address":address,
-            "company_phone":company_phone,
-            "company_contact_person":company_contact_person,
-            "company_contact_person_title" : company_contact_person_title,
-            "company_website":company_website,
-            "company_email":company_email,
-            "company_linkedin":company_linkedin,
+            "company_country": response.xpath(COMPANY_COUNTRY_SELECTOR).extract(),
+            "company_name": response.css(COMPANY_SELECTOR).extract(),
+            "no_of_employees": response.xpath(no_of_employees_SELECTOR).extract(),
+            "address":response.xpath(ADDRESS_SELECTOR).extract(),
+            "company_phone":response.xpath(COMPANY_CONTACT_NUMBER_SELECTOR).extract(),
+            "company_contact_person":response.xpath(COMPANY_CONTACT_PERSON_SELECTOR).extract_first(),
+            "company_contact_person_title" : response.xpath(COMPANY_CONTACT_PERSON_TITLE_SELECTOR).extract(),
+            "company_website":response.xpath(COMPANY_WEBSITE_SELECTOR).extract(),
+            "company_email":response.xpath(COMPANY_EMAIL_SELECTOR).extract(),
+            "company_linkedin":response.css(COMPANY_LINKEDIN_SELECTOR).extract(),
             "contact_person_name": contact_person_name,
             "contact_person_title": contact_person_title,
             "contact_person_phone": contact_person_phone,
